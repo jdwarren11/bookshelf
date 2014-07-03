003374f62fe4584d2926bf0eec03e682f507a6ae
Jeremy Warren <jdwarren11@gmail.com>
2014-07-03 15:46:17 -0500

    Add styling to homepage

--- server.rb
+++ server.rb
@@ -32,7 +32,6 @@ get '/' do
     @bookshelf = BOOK.orm.get_user_bookshelf_by_id(session[:user_id])
     @bookshelf.each do |x|
       api_id = x["api_id"]
-    
       picture = BOOK.orm.get_pic_by_api(api_id)
       @pics <<picture
     end
